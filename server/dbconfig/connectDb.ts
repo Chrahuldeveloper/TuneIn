1401cf4d3bf5b30ad6d320b2f4e2b0d766a47c0e
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-03 18:20:18 +0530

    db connected

---
+++ server/dbconfig/connectDb.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+
+const db = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "mydb1",
+  password: "1234",
+  port: 5432,
+});
+
+module.exports = {
+  db,
+};
