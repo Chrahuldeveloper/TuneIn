7be8c0702fc506739d52115576a80400b89d903f
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-04 17:16:09 +0530

    done

--- server/routes/SaveDetails.js
+++ server/routes/SaveDetails.js
@@ -1,15 +1,13 @@
 const { Router } = require("express");
-const saveuserRouter = Router();
+const saveDetailsRouter = Router();
 const dbclient = require("../dbconfig/connectDb");
-saveuserRouter.post("/api/save", async (req, res) => {
+saveDetailsRouter.post("/api/save", async (req, res) => {
   try {
     const { name, email } = req.body;
-
     await dbclient.query("INSERT INTO users (name, email) VALUES ($1, $2)", [
       name,
       email,
     ]);
-
     res.status(201).json({ message: "✅ User saved successfully" });
   } catch (error) {
     console.error("❌ Error saving user:", error);
@@ -17,4 +15,5 @@ saveuserRouter.post("/api/save", async (req, res) => {
   }
 });
 
-module.exports = saveuserRouter 
+
+module.exports = saveDetailsRouter;
