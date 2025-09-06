d7d681833def2190f1eda0fac61f263f724c5b73
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-07 01:06:51 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Widget from "./pages/Widget";
 
 function App() {
   return (
     <Routes>
       <Route element={<Home />} path="/" />
       <Route element={<Dashboard />} path="/dashboard" />
+      <Route element={<Widget />} path="/:username/widget/:id/:widgetname" />
     </Routes>
   );
 }
