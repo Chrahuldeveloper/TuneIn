49c4db35f4e13dabd2fae36c17b88f4d4cb72985
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-15 18:19:12 +0530

    deployed db

--- server/dbconfig/connectDb.js
+++ server/dbconfig/connectDb.js
@@ -1,11 +1,23 @@
-const { Pool } =  require("pg");
+require("dotenv").config();
+const { Pool } = require("pg");
 const db = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mydb1",
-  password: "1234",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
+db.query("SELECT NOW()", (err, res) => {
+  if (res) {
+    console.log("db Connected");
+  } else {
+    console.error("Connection error", err);
+  }
+  db.end();
+});
 
-module.exports = db
+module.exports = db;
