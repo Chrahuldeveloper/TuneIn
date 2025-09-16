8a12a2db75336067e5a2463fd614312f2a7e5e78
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-16 23:06:20 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 
+// {source:"coding"}
 export default defineConfig({
-  plugins: [react(), mkcert({source:"coding"})],
+  plugins: [react(), mkcert()],
   server: {
     host: "127.0.0.1",
     port: 8000,
-    // strictPort: true,
-    https : true
+    strictPort: true,
+    // https : true
   },
 });
