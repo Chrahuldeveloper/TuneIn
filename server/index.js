fcd1b22229ca30e8e3c36d9a38e82588a10f6206
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-09-23 20:15:30 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://tune-in-inky.vercel.app",
+  origin: "https://spotifytunein.vercel.app",
   credentials: true
 }));
 
